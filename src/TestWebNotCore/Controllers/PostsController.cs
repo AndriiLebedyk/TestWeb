385e8e3d361ee6c02ba03a4d70b833133caa8688
Andrii Lebedyk <andrey.lebedyk@gmail.com>
2019-03-26 17:21:49 +0200

    Release context of database after finishing of a request.

--- src/TestWebNotCore/Controllers/PostsController.cs
+++ src/TestWebNotCore/Controllers/PostsController.cs
@@ -12,16 +12,22 @@ namespace TestWebNotCore.Controllers
         private readonly ApplicationDbContext m_db = new ApplicationDbContext();
 
         public ActionResult Index() {
-            var lastMonth = DateTime.Now.AddMonths(-1);
-            return View(m_db.Posts.Where(x => x.Date >= lastMonth).ToList());
+            using(var m_db = new ApplicationDbContext())
+            {
+                var lastMonth = DateTime.Now.AddMonths(-1);
+                return View(m_db.Posts.Where(x => x.Date >= lastMonth).ToList());
+            }
         }
 
         public ActionResult Details(int? id)
         {
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            using (var m_db = new ApplicationDbContext())
+            {
+                if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
-            Post post = m_db.Posts.Find(id);
-            return post == null ? (ActionResult) HttpNotFound() : View(post);
+                Post post = m_db.Posts.Find(id);
+                return post == null ? (ActionResult)HttpNotFound() : View(post);
+            }
         }
 
         public ActionResult Create() => View();
@@ -30,20 +36,25 @@ namespace TestWebNotCore.Controllers
         public ActionResult Create([Bind(Include = "Id,Title,Body")] Post post)
         {
             if (!ModelState.IsValid) return View(post);
-
-            post.AuthorName = User.Identity.Name;
-            post.Date = DateTime.Now;
-            m_db.Posts.Add(post);
-            m_db.SaveChanges();
-            return RedirectToAction("Index");
+            using (var m_db = new ApplicationDbContext())
+            {
+                post.AuthorName = User.Identity.Name;
+                post.Date = DateTime.Now;
+                m_db.Posts.Add(post);
+                m_db.SaveChanges();
+                return RedirectToAction("Index");
+            }
         }
         [HttpPost]
         public ActionResult Delete(int id)
         {
-            Post post = m_db.Posts.Find(id);
-            m_db.Posts.Remove(post);
-            m_db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            using(var m_db = new ApplicationDbContext())
+            {
+                Post post = m_db.Posts.Find(id);
+                m_db.Posts.Remove(post);
+                m_db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
         }
     }
 }
